28c2626ff770de69a9cf9ca2f1f9189b08ab30b8
Jonathan Lindgren <jonathan@novastream.se>
2023-12-11 22:28:57 +0100

    Update 1.1.5
    Fixed menu bug
    Re-arranged file structure

---
+++ module/scripts/BeforeInstall.php
@@ -0,0 +1,18 @@
+<?php
+
+class BeforeInstall
+{
+    protected $container;
+
+    public function run($container)
+    {
+        $this->container = $container;
+    }
+
+    protected function clearCache()
+    {
+        try {
+            $this->container->get('dataManager')->clearCache();
+        } catch (\Exception $e) {}
+    }
+}
